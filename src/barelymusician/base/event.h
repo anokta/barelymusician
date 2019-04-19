45d9067a1c3509708b58841827c63a9384f0923a
alper gungormusler <anokta@gmail.com>
2019-04-18 22:27:49 -0400

    implements event template to be used in sequencer.

---
+++ src/barelymusician/base/event.h
@@ -0,0 +1,56 @@
+#ifndef BARELYAPI_BASE_EVENT_H_
+#define BARELYAPI_BASE_EVENT_H_
+
+#include <functional>
+#include <vector>
+
+#include "barelymusician/base/module.h"
+
+namespace barelyapi {
+
+// Argument agnostic event that allows a triggering mechanism for all the
+// registered callbacks.
+template <typename... ArgumentTypes>
+class Event : public Module {
+ public:
+  // Event callback signature.
+  using Callback = std::function<void(ArgumentTypes...)>;
+
+  // Implements |Module|.
+  void Reset() override;
+
+  // Registers callback for the event.
+  //
+  // @param callback Event callback.
+  void Register(Callback&& callback);
+
+  // Triggers the event for all the registered callbacks.
+  //
+  // @param arguments List of arguments to trigger the callbacks with.
+  void Trigger(ArgumentTypes... arguments) const;
+
+ private:
+  // List of callbacks.
+  std::vector<Callback> callbacks_;
+};
+
+template <typename... ArgumentTypes>
+void Event<ArgumentTypes...>::Reset() {
+  callbacks_.clear();
+}
+
+template <typename... ArgumentTypes>
+void Event<ArgumentTypes...>::Register(Callback&& callback) {
+  callbacks_.push_back(std::move(callback));
+}
+
+template <typename... ArgumentTypes>
+void Event<ArgumentTypes...>::Trigger(ArgumentTypes... arguments) const {
+  for (const auto& callback : callbacks_) {
+    callback(arguments...);
+  }
+}
+
+}  // namespace barelyapi
+
+#endif  // BARELYAPI_BASE_EVENT_H_
---
+++ src/barelymusician/base/event_test.cpp
@@ -0,0 +1,74 @@
+#include "barelymusician/base/event.h"
+
+#include "gtest/gtest.h"
+
+namespace barelyapi {
+namespace {
+
+// Tests that registering a single callback to event gets triggered as expected.
+TEST(EventTest, RegisterSingleCallback) {
+  const int kNumEventTriggers = 5;
+
+  Event<int> event;
+
+  int result = 0;
+  const auto negate_callback = [&result](int value) { result = -value; };
+  event.Register(negate_callback);
+
+  for (int i = 0; i < kNumEventTriggers; ++i) {
+    event.Trigger(i);
+    EXPECT_EQ(-i, result);
+  }
+}
+
+// Tests that registering multiple callbacks callback to event get triggered as
+// expected.
+TEST(EventTest, RegisterMultipleCallbacks) {
+  const float kValue1 = 2.5f;
+  const float kValue2 = -12.0f;
+
+  Event<float, float> event;
+
+  float sum = 0.0f;
+  const auto sum_callback = [&sum](float value1, float value2) {
+    sum = value1 + value2;
+  };
+  event.Register(sum_callback);
+
+  float difference = 0.0f;
+  const auto difference_callback = [&difference](float value1, float value2) {
+    difference = value1 - value2;
+  };
+  event.Register(difference_callback);
+
+  // Verify that triggering the event calls both callbacks.
+  event.Trigger(kValue1, kValue2);
+  EXPECT_FLOAT_EQ(kValue1 + kValue2, sum);
+  EXPECT_FLOAT_EQ(kValue1 - kValue2, difference);
+}
+
+// Tests that resetting event clears the registered callbacks as expected.
+TEST(EventTest, Reset) {
+  const float kInitValue = 0.0f;
+  const float kSetValue = 20.0f;
+
+  Event<float> event;
+
+  // Trigger should be no-op when the callback is not registered.
+  float value = kInitValue;
+  event.Trigger(kSetValue);
+  EXPECT_FLOAT_EQ(kInitValue, value);
+
+  // Register the callback, trigger should set the value now.
+  event.Register([&value](float set_value) { value = set_value; });
+  event.Trigger(kSetValue);
+  EXPECT_FLOAT_EQ(kSetValue, value);
+
+  // Reset the event, trigger should be no-op again.
+  event.Reset();
+  event.Trigger(kInitValue);
+  EXPECT_FLOAT_EQ(kSetValue, value);
+}
+
+}  // namespace
+}  // namespace barelyapi

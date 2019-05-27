4321e2461816afbfafc00e0e8bc1c76456f6c3ae
alper gungormusler <anokta@gmail.com>
2019-05-26 22:28:56 -0400

    initial unity project.
    fixed #49.

---
+++ unity/Assets/BarelyMusician/Scripts/BarelyMusician.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace BarelyApi {
+
+  // Main |BarelyMusician| class that communicates with the native code.
+  public class BarelyMusician {
+    // Singleton instance.
+    public static BarelyMusician Instance {
+      get {
+        if (instance == null) {
+          instance = new BarelyMusician();
+        }
+        return instance;
+      }
+    }
+    private static BarelyMusician instance = null;
+
+    // Registers sequencer beat callback.
+    public void RegisterSequencerBeatCallback(BeatCallback beatCallback) {
+      RegisterBeatCallback(Marshal.GetFunctionPointerForDelegate(beatCallback));
+    }
+
+    // Sets sequencer number of bars per section.
+    public void SetSequencerNumBars(int numBars) {
+      SetNumBars(numBars);
+    }
+
+    // Sets sequencer number of beats per bar.
+    public void SetSequencerNumBeats(int numBeats) {
+      SetNumBeats(numBeats);
+    }
+
+    // Sets sequencer tempo.
+    public void SetSequencerTempo(float tempo) {
+      SetTempo(tempo);
+    }
+
+    // Updates sequencer.
+    public void UpdateSequencer() {
+      Update();
+    }
+
+    // Constructs new |BarelyMusician| with Unity audio settings.
+    BarelyMusician() {
+      var config = AudioSettings.GetConfiguration();
+      int sampleRate = config.sampleRate;
+      int numChannels = (int)config.speakerMode;
+      int numFrames = config.dspBufferSize;
+      Initialize(sampleRate, numChannels, numFrames);
+    }
+
+    // Shuts down |BarelyMusician|.
+    ~BarelyMusician() {
+      Shutdown();
+    }
+
+#if !UNITY_EDITOR && UNITY_IOS
+  private const string pluginName = "__Internal";
+#else
+    private const string pluginName = "barelymusicianunity";
+#endif  // !UNITY_EDITOR && UNITY_IOS
+
+    [DllImport(pluginName)]
+    private static extern void Initialize(int sampleRate, int numChannels, int numFrames);
+
+    [DllImport(pluginName)]
+    private static extern void RegisterBeatCallback(IntPtr beatCallback);
+
+    [DllImport(pluginName)]
+    private static extern void SetNumBars(int numBars);
+
+    [DllImport(pluginName)]
+    private static extern void SetNumBeats(int numBeats);
+
+    [DllImport(pluginName)]
+    private static extern void SetTempo(float tempo);
+
+    [DllImport(pluginName)]
+    private static extern void Shutdown();
+
+    [DllImport(pluginName)]
+    private static extern void Update();
+  }
+
+}  // namespace BarelyApi
---
+++ unity/Assets/BarelyMusician/Scripts/Sequencer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BarelyApi {
+
+  // Beat callback.
+  public delegate void BeatCallback(int section, int bar, int beat);
+
+  // Beat sequencer.
+  [RequireComponent(typeof(AudioSource))]
+  public class Sequencer : MonoBehaviour {
+    // Tempo (BPM).
+    [Range(0.0f, 480.0f)]
+    public float tempo = 120.0f;
+
+    // Number of bars per section.
+    [Range(0, 16)]
+    public int numBars = 0;
+
+    // Number of beats per bar.
+    [Range(0, 16)]
+    public int numBeats = 0;
+
+    void Awake() {
+      BarelyMusician.Instance.RegisterSequencerBeatCallback(DebugBeatCallback);
+    }
+
+    void Update() {
+      UpdateSequencer();
+    }
+
+    void OnAudioFilterRead(float[] data, int channels) {
+      BarelyMusician.Instance.UpdateSequencer();
+    }
+
+    private void DebugBeatCallback(int section, int bar, int beat) {
+      Debug.Log("Beat: " + section + "." + bar + "." + beat);
+    }
+
+    private void UpdateSequencer() {
+      var barelymusician = BarelyMusician.Instance;
+      barelymusician.SetSequencerTempo(tempo);
+      barelymusician.SetSequencerNumBars(numBars);
+      barelymusician.SetSequencerNumBeats(numBeats);
+    }
+  }
+
+}  // namespace BarelyApi

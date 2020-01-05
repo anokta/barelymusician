d25f1ea760728a4be5a166d7718da1b15c44750f
alper gungormusler <anokta@gmail.com>
2020-01-04 20:16:11 -0500

    initial refactor to ensemble replaced by musician.
    + a lot of things still wip, but moving towards the target solution

---
+++ examples/demo/musician_demo.cpp
@@ -0,0 +1,293 @@
+#include <algorithm>
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <thread>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include "audio_output/pa_audio_output.h"
+#include "barelymusician/base/clock.h"
+#include "barelymusician/base/constants.h"
+#include "barelymusician/base/logging.h"
+#include "barelymusician/base/random.h"
+#include "barelymusician/dsp/dsp_utils.h"
+#include "barelymusician/instrument/instrument.h"
+#include "barelymusician/musician/musician.h"
+#include "barelymusician/musician/note.h"
+#include "barelymusician/musician/note_utils.h"
+#include "instruments/basic_drumkit_instrument.h"
+#include "instruments/basic_synth_instrument.h"
+#include "util/input_manager/win_console_input.h"
+#include "util/wav_file.h"
+
+namespace {
+
+using ::barelyapi::Clock;
+using ::barelyapi::Instrument;
+using ::barelyapi::Musician;
+using ::barelyapi::Note;
+using ::barelyapi::OscillatorType;
+using ::barelyapi::Random;
+using ::barelyapi::SamplesFromBeats;
+using ::barelyapi::examples::BasicDrumkitInstrument;
+using ::barelyapi::examples::BasicSynthInstrument;
+using ::barelyapi::examples::BasicSynthInstrumentParam;
+using ::barelyapi::examples::PaAudioOutput;
+using ::barelyapi::examples::WavFile;
+using ::barelyapi::examples::WinConsoleInput;
+
+// System audio settings.
+const int kSampleRate = 48000;
+const int kNumChannels = 2;
+const int kNumFrames = 512;
+
+const float kSampleInterval = 1.0f / static_cast<float>(kSampleRate);
+
+// Sequencer settings.
+const float kTempo = 124.0f;
+const int kNumBars = 4;
+const int kNumBeats = 3;
+
+// Ensemble settings.
+const float kRootNote = barelyapi::kNoteIndexD3;
+const int kNumInstrumentVoices = 8;
+
+std::unique_ptr<BasicSynthInstrument> BuildSynthInstrument(OscillatorType type,
+                                                           float gain,
+                                                           float attack,
+                                                           float release) {
+  auto synth_instrument = std::make_unique<BasicSynthInstrument>(
+      kSampleInterval, kNumInstrumentVoices);
+  synth_instrument->SetFloatParam(BasicSynthInstrumentParam::kOscillatorType,
+                                  static_cast<float>(type));
+  synth_instrument->SetFloatParam(BasicSynthInstrumentParam::kGain, gain);
+  synth_instrument->SetFloatParam(BasicSynthInstrumentParam::kEnvelopeAttack,
+                                  attack);
+  synth_instrument->SetFloatParam(BasicSynthInstrumentParam::kEnvelopeRelease,
+                                  release);
+  return std::move(synth_instrument);
+}
+
+void ComposeChord(float root_note_index, const std::vector<float>& scale,
+                  float intensity, int harmonic, std::vector<Note>* notes) {
+  const auto add_chord_note = [&](float index) {
+    const float note_index =
+        root_note_index + barelyapi::GetNoteIndex(scale, index);
+    notes->push_back({note_index, intensity, 0, 0.0f, 1.0f});
+  };
+  const float start_note = static_cast<float>(harmonic);
+  add_chord_note(start_note);
+  add_chord_note(start_note + 2.0f);
+  add_chord_note(start_note + 4.0f);
+  add_chord_note(start_note + 7.0f);
+}
+
+void ComposeLine(float root_note_index, const std::vector<float>& scale,
+                 float intensity, int bar, int beat, int harmonic,
+                 std::vector<Note>* notes) {
+  const float start_note = static_cast<float>(harmonic);
+  const float note_offset = static_cast<float>(beat);
+  const auto add_note = [&](float index, float offset_beats, float end_beat) {
+    notes->push_back({root_note_index + barelyapi::GetNoteIndex(scale, index),
+                      intensity, 0, offset_beats, end_beat});
+  };
+  if (beat % 2 == 1) {
+    add_note(start_note, 0.0f, 0.25f);
+    add_note(start_note - note_offset, 0.33f, 0.33f);
+    add_note(start_note, 0.66f, 0.33f);
+  } else {
+    add_note(start_note + note_offset, 0.0f, 0.25f);
+  }
+  if (beat % 2 == 0) {
+    add_note(start_note - note_offset, 0.0f, 0.05f);
+    add_note(start_note - 2.0f * note_offset, 0.5f, 0.05f);
+  }
+  if (beat + 1 == kNumBeats && bar % 2 == 1) {
+    add_note(start_note + 2.0f * note_offset, 0.25f, 0.125f);
+    add_note(start_note - 2.0f * note_offset, 0.75f, 0.125f);
+    add_note(start_note + 2.0f * note_offset, 0.5f, 0.25f);
+  }
+}
+
+void ComposeDrums(int bar, int beat, std::vector<Note>* notes) {
+  const auto get_beat = [](int step) {
+    return barelyapi::GetBeat(step, barelyapi::kNumSixteenthNotesPerBeat);
+  };
+  // Kick.
+  if (beat % 2 == 0) {
+    notes->push_back(
+        {barelyapi::kNoteIndexKick, 1.0f, 0, get_beat(0), get_beat(2)});
+    if (bar % 2 == 1 && beat == 0) {
+      notes->push_back(
+          {barelyapi::kNoteIndexKick, 1.0f, 0, get_beat(2), get_beat(2)});
+    }
+  }
+  // Snare.
+  if (beat % 2 == 1) {
+    notes->push_back(
+        {barelyapi::kNoteIndexSnare, 1.0f, 0, get_beat(0), get_beat(2)});
+  }
+  if (beat + 1 == kNumBeats) {
+    notes->push_back(
+        {barelyapi::kNoteIndexSnare, 0.75f, 0, get_beat(2), get_beat(2)});
+    if (bar + 1 == kNumBars) {
+      notes->push_back(
+          {barelyapi::kNoteIndexSnare, 1.0f, 0, get_beat(1), get_beat(1)});
+      notes->push_back(
+          {barelyapi::kNoteIndexSnare, 0.75f, 0, get_beat(3), get_beat(1)});
+    }
+  }
+  // Hihat Closed.
+  notes->push_back({barelyapi::kNoteIndexHihatClosed,
+                    Random::Uniform(0.5f, 0.75f), 0, get_beat(0), get_beat(2)});
+  notes->push_back({barelyapi::kNoteIndexHihatClosed,
+                    Random::Uniform(0.25f, 0.75f), 0, get_beat(2),
+                    get_beat(2)});
+  // Hihat Open.
+  if (beat + 1 == kNumBeats) {
+    if (bar + 1 == kNumBars) {
+      notes->push_back({barelyapi::kNoteIndexHihatOpen, 0.75f, beat,
+                        get_beat(1), get_beat(1)});
+    } else if (bar % 2 == 0) {
+      notes->push_back({barelyapi::kNoteIndexHihatOpen, 0.75f, beat,
+                        get_beat(3), get_beat(1)});
+    }
+  }
+  if (beat == 0 && bar == 0) {
+    notes->push_back(
+        {barelyapi::kNoteIndexHihatOpen, 1.0f, 0, get_beat(0), get_beat(2)});
+  }
+}
+
+}  // namespace
+
+int main(int argc, char* argv[]) {
+  PaAudioOutput audio_output;
+  WinConsoleInput input_manager;
+
+  Musician musician(kSampleRate);
+  musician.SetNumBars(kNumBars);
+  musician.SetNumBeats(kNumBeats);
+  musician.SetTempo(kTempo);
+
+  const std::vector<int> progression = {0, 3, 4, 0};
+  const std::vector<float> scale(std::begin(barelyapi::kMajorScale),
+                                 std::end(barelyapi::kMajorScale));
+
+  // Ensemble.
+  Musician::Ensemble& ensemble = musician.ensemble();
+  ensemble.section_composer_callback = [](int section) -> int {
+    return section;
+  };
+  ensemble.bar_composer_callback = [&progression](int bar, int) -> int {
+    return progression[bar % progression.size()];
+  };
+
+  // Synth instruments.
+  auto chords_instrument =
+      BuildSynthInstrument(OscillatorType::kSine, 0.125f, 0.125f, 0.125f);
+  auto chords_2_instrument =
+      BuildSynthInstrument(OscillatorType::kNoise, 0.05f, 0.5f, 0.025f);
+
+  const auto chords_beat_composer_callback =
+      std::bind(ComposeChord, kRootNote, scale, 0.5f, std::placeholders::_4,
+                std::placeholders::_5);
+
+  ensemble.performers.emplace_back(chords_instrument.get(),
+                                   chords_beat_composer_callback);
+  ensemble.performers.emplace_back(chords_2_instrument.get(),
+                                   chords_beat_composer_callback);
+
+  auto line_instrument =
+      BuildSynthInstrument(OscillatorType::kSaw, 0.125f, 0.0025f, 0.125f);
+  auto line_2_instrument =
+      BuildSynthInstrument(OscillatorType::kSquare, 0.15f, 0.05f, 0.05f);
+
+  const auto line_beat_composer_callback =
+      std::bind(ComposeLine, kRootNote - barelyapi::kNumSemitones, scale, 1.0f,
+                std::placeholders::_1, std::placeholders::_2,
+                std::placeholders::_4, std::placeholders::_5);
+  const auto line_2_beat_composer_callback = std::bind(
+      ComposeLine, kRootNote, scale, 1.0f, std::placeholders::_1,
+      std::placeholders::_2, std::placeholders::_4, std::placeholders::_5);
+
+  ensemble.performers.emplace_back(line_instrument.get(),
+                                   line_beat_composer_callback);
+  ensemble.performers.emplace_back(line_2_instrument.get(),
+                                   line_2_beat_composer_callback);
+
+  // Drumkit instrument.
+  std::unordered_map<float, std::string> drumkit_map;
+  drumkit_map[barelyapi::kNoteIndexKick] = "data/audio/drums/basic_kick.wav";
+  drumkit_map[barelyapi::kNoteIndexSnare] = "data/audio/drums/basic_snare.wav";
+  drumkit_map[barelyapi::kNoteIndexHihatClosed] =
+      "data/audio/drums/basic_hihat_closed.wav";
+  drumkit_map[barelyapi::kNoteIndexHihatOpen] =
+      "data/audio/drums/basic_hihat_open.wav";
+  auto drumkit_instrument =
+      std::make_unique<BasicDrumkitInstrument>(kSampleInterval);
+  std::vector<WavFile> drumkit_files;
+  for (const auto& it : drumkit_map) {
+    drumkit_files.emplace_back();
+    auto& drumkit_file = drumkit_files.back();
+    CHECK(drumkit_file.Load(it.second));
+    drumkit_instrument->Add(it.first, drumkit_file);
+  }
+
+  const auto drumkit_beat_composer_callback =
+      std::bind(ComposeDrums, std::placeholders::_1, std::placeholders::_2,
+                std::placeholders::_5);
+
+  ensemble.performers.emplace_back(drumkit_instrument.get(),
+                                   drumkit_beat_composer_callback);
+
+  // Audio process callback.
+  std::vector<float> temp_buffer(kNumChannels * kNumFrames);
+  int timestamp = 0;
+  const auto process_callback = [&musician, &ensemble, &temp_buffer,
+                                 &timestamp](float* output) {
+    musician.Update(kNumFrames, timestamp);
+
+    std::fill_n(output, kNumChannels * kNumFrames, 0.0f);
+    for (const auto& it : ensemble.performers) {
+      it.instrument->ProcessScheduled(temp_buffer.data(), kNumChannels,
+                                      kNumFrames, timestamp);
+      std::transform(temp_buffer.begin(), temp_buffer.end(), output, output,
+                     std::plus<float>());
+    }
+    timestamp += kNumFrames;
+  };
+  audio_output.SetProcessCallback(process_callback);
+
+  // Key down callback.
+  bool quit = false;
+  const auto key_down_callback = [&quit](const WinConsoleInput::Key& key) {
+    if (static_cast<int>(key) == 27) {
+      // ESC pressed, quit the app.
+      quit = true;
+      return;
+    }
+  };
+  input_manager.SetKeyDownCallback(key_down_callback);
+
+  // Start the demo.
+  LOG(INFO) << "Starting audio stream";
+
+  input_manager.Initialize();
+  audio_output.Start(kSampleRate, kNumChannels, kNumFrames);
+
+  while (!quit) {
+    input_manager.Update();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+  }
+
+  // Stop the demo.
+  LOG(INFO) << "Stopping audio stream";
+
+  audio_output.Stop();
+  input_manager.Shutdown();
+
+  return 0;
+}
---
+++ src/barelymusician/musician/musician.h
@@ -0,0 +1,151 @@
+#ifndef BARELYMUSICIAN_MUSICIAN_MUSICIAN_H_
+#define BARELYMUSICIAN_MUSICIAN_MUSICIAN_H_
+
+#include <functional>
+#include <utility>
+#include <vector>
+
+#include "barelymusician/base/clock.h"
+#include "barelymusician/base/logging.h"
+#include "barelymusician/dsp/dsp_utils.h"
+#include "barelymusician/instrument/instrument.h"
+#include "barelymusician/message/message_buffer.h"
+#include "barelymusician/musician/score.h"
+
+namespace barelyapi {
+
+class Musician {
+ public:
+  struct Ensemble {
+    // Section composer callback signature.
+    using SectionComposerCallback = std::function<int(int section)>;
+
+    // Bar composer callback signature.
+    using BarComposerCallback = std::function<int(int bar, int section_type)>;
+
+    // Beat composer callback signature.
+    using BeatComposerCallback =
+        std::function<void(int bar, int beat, int section_type, int harmonic,
+                           std::vector<Note>* notes)>;
+
+    // Section composer callback.
+    SectionComposerCallback section_composer_callback;
+
+    // Bar composer callback.
+    BarComposerCallback bar_composer_callback;
+
+    struct Performer {
+      Performer(Instrument* instrument,
+                BeatComposerCallback beat_composer_callback)
+          : instrument(instrument),
+            beat_composer_callback(beat_composer_callback) {}
+
+      Instrument* instrument;
+      BeatComposerCallback beat_composer_callback;
+      Score score;
+    };
+
+    // List of performers.
+    std::vector<Performer> performers;
+  };
+
+  // TODO: get num_bars from section composer.
+  explicit Musician(int sample_rate)
+      : clock_(sample_rate), section_(0), bar_(0), beat_(0) {
+    clock_.SetBeatCallback([this](int beat, int leftover_samples) {
+      // Update transport.
+      beat_ = beat % num_beats_;
+      bar_ = beat / num_beats_;
+      section_ = bar_ / num_bars_;
+      bar_ %= num_bars_;
+
+      if (beat_ == 0) {
+        if (bar_ == 0) {
+          // Compose next section.
+          section_type_ = ensemble_.section_composer_callback(section_);
+        }
+        // Compose next bar.
+        harmonic_ = ensemble_.bar_composer_callback(bar_, section_type_);
+      }
+
+      // Update performers.
+      for (Ensemble::Performer& performer : ensemble_.performers) {
+        // Compose next beat notes.
+        temp_notes_.clear();
+        performer.beat_composer_callback(bar_, beat_, section_type_, harmonic_,
+                                         &temp_notes_);
+        for (Note& note : temp_notes_) {
+          // TODO: inefficient?!
+          note.start_beat = beat;
+          performer.score.AddNote(note);
+        }
+      }
+    });
+  }
+
+  // TODO: get this from section type.
+  void SetNumBars(int num_bars) { num_bars_ = num_bars; }
+
+  void SetNumBeats(int num_beats) { num_beats_ = num_beats; }
+
+  void SetTempo(float tempo) { tempo_ = tempo; }
+
+  // TODO: timestamp not necessary?
+  void Update(int num_samples, int timestamp) {
+    // TODO: is this efficient?
+    if (clock_.GetTempo() != tempo_) {
+      clock_.SetTempo(tempo_);
+    }
+
+    const float start_position = clock_.GetPosition();
+    const int start_beat = clock_.GetBeat();
+    const int leftover_samples = clock_.GetLeftoverSamples();
+    clock_.Update(num_samples);
+    const float end_position = clock_.GetPosition();
+
+    const int num_samples_per_beat = clock_.GetNumSamplesPerBeat();
+    for (Ensemble::Performer& performer : ensemble_.performers) {
+      // TODO: store note events somewhere to avoid timestamp.
+      const auto notes =
+          performer.score.GetIterator(start_position, end_position);
+      for (auto it = notes.begin; it != notes.end; ++it) {
+        const int note_on_timestamp =
+            timestamp + (it->start_beat - start_beat) * num_samples_per_beat +
+            SamplesFromBeats(it->offset_beats, num_samples_per_beat) -
+            leftover_samples;
+        performer.instrument->NoteOnScheduled(it->index, it->intensity,
+                                              note_on_timestamp);
+        const int note_off_timestamp =
+            note_on_timestamp +
+            SamplesFromBeats(it->duration_beats, num_samples_per_beat);
+        performer.instrument->NoteOffScheduled(it->index, note_off_timestamp);
+      }
+    }
+  }
+
+  Ensemble& ensemble() { return ensemble_; }
+  const Ensemble& ensemble() const { return ensemble_; }
+
+ private:
+  Clock clock_;
+  Ensemble ensemble_;
+
+  int section_;
+  int bar_;
+  int beat_;
+
+  int num_bars_;
+  int num_beats_;
+
+  int section_type_;
+  int harmonic_;
+
+  float tempo_;
+
+  // TODO: not necessary, use Score instead?
+  std::vector<Note> temp_notes_;
+};
+
+}  // namespace barelyapi
+
+#endif  // BARELYMUSICIAN_MUSICIAN_MUSICIAN_H_

c4d74b77956ec8b514cf2257c4225733586d2c90
alper gungormusler <anokta@gmail.com>
2020-11-03 20:35:32 -0500

    removes module.

--- barelymusician/dsp/filter.h
+++ barelymusician/dsp/filter.h
@@ -1,18 +1,22 @@
 #ifndef BARELYMUSICIAN_DSP_PROCESSOR_H_
 #define BARELYMUSICIAN_DSP_PROCESSOR_H_
 
-#include "barelymusician/dsp/module.h"
-
 namespace barelyapi {
 
 // Unit filter interface that processes per sample PCM input.
-class Filter : public Module {
+class Filter {
  public:
+  // Base destructor to ensure the derived classes get destroyed properly.
+  virtual ~Filter() = default;
+
   // Filters the next input sample.
   //
   // @param input Input sample.
   // @return Filtered output sample.
   virtual float Next(float input) = 0;
+
+  // Resets module state.
+  virtual void Reset() = 0;
 };
 
 }  // namespace barelyapi
--- barelymusician/dsp/generator.h
+++ barelymusician/dsp/generator.h
@@ -1,17 +1,21 @@
 #ifndef BARELYMUSICIAN_DSP_GENERATOR_H_
 #define BARELYMUSICIAN_DSP_GENERATOR_H_
 
-#include "barelymusician/dsp/module.h"
-
 namespace barelyapi {
 
 // Unit generator interface that produces per sample PCM output.
-class Generator : public Module {
+class Generator {
  public:
+  // Base destructor to ensure the derived classes get destroyed properly.
+  virtual ~Generator() = default;
+
   // Generates the next output sample.
   //
   // @return Next output sample.
   virtual float Next() = 0;
+
+  // Resets module state.
+  virtual void Reset() = 0;
 };
 
 }  // namespace barelyapi

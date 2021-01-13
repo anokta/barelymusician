8a8e3cad628a4f7523dad78c4b17aff24eafe39a
alper gungormusler <anokta@gmail.com>
2021-01-12 21:54:28 -0500

    minor refactor in score_test.

--- barelymusician/engine/score_test.cpp
+++ barelymusician/engine/score_test.cpp
@@ -19,24 +19,30 @@ TEST(ScoreTest, SingleNote) {
 
   // Add note.
   const int event_id =
-      score.AddNoteEvent(kPosition, kPosition + kDuration, kPitch, kIntensity);
+      score.AddNoteEvent(kPosition, kDuration, kPitch, kIntensity);
   EXPECT_FALSE(score.IsEmpty());
 
+  int num_note_ons = 0;
+  int num_note_offs = 0;
   score.ForEachEventInRange(
-      kPosition, kPosition + kDuration,
+      kPosition, kPosition + kDuration + 1.0,
       [&](double position, const InstrumentData& data) {
         if (std::holds_alternative<NoteOn>(data)) {
+          ++num_note_ons;
           const auto note_on = std::get<NoteOn>(data);
           EXPECT_DOUBLE_EQ(position, kPosition);
           EXPECT_FLOAT_EQ(note_on.pitch, kPitch);
           EXPECT_FLOAT_EQ(note_on.intensity, kIntensity);
         } else {
           ASSERT_TRUE(std::holds_alternative<NoteOff>(data));
+          ++num_note_offs;
           const auto note_off = std::get<NoteOff>(data);
           EXPECT_DOUBLE_EQ(position, kPosition + kDuration);
           EXPECT_FLOAT_EQ(note_off.pitch, kPitch);
         }
       });
+  EXPECT_EQ(num_note_ons, 1);
+  EXPECT_EQ(num_note_offs, 1);
 
   EXPECT_TRUE(score.RemoveEvent(event_id));
   EXPECT_TRUE(score.IsEmpty());
@@ -48,7 +54,6 @@ TEST(ScoreTest, SingleNote) {
 // Tests that the score stores multiple notes as expected.
 TEST(ScoreTest, MultipleNotes) {
   const int kNumNotes = 10;
-  const double kDuration = 0.5;
   const float kIntensity = 0.25f;
 
   Score score;
@@ -56,7 +61,7 @@ TEST(ScoreTest, MultipleNotes) {
 
   // Add notes.
   for (int i = 0; i < kNumNotes; ++i) {
-    score.AddNoteEvent(static_cast<double>(i), kDuration, static_cast<float>(i),
+    score.AddNoteEvent(static_cast<double>(i), 1.0, static_cast<float>(i),
                        kIntensity);
   }
   EXPECT_FALSE(score.IsEmpty());
@@ -74,13 +79,13 @@ TEST(ScoreTest, MultipleNotes) {
         } else {
           ASSERT_TRUE(std::holds_alternative<NoteOff>(data));
           const auto note_off = std::get<NoteOff>(data);
-          EXPECT_DOUBLE_EQ(position,
-                           static_cast<double>(note_off.pitch) + kDuration);
+          EXPECT_DOUBLE_EQ(position, static_cast<double>(note_off.pitch) + 1.0);
           ++num_note_offs;
         }
       });
   EXPECT_EQ(num_note_ons, kNumNotes);
-  EXPECT_EQ(num_note_offs, kNumNotes);
+  // Last note off should be out of range.
+  EXPECT_EQ(num_note_offs, kNumNotes - 1);
 
   score.RemoveAllEvents();
   EXPECT_TRUE(score.IsEmpty());

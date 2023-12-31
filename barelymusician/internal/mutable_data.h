ed1124b365659a74eaad07c3536cbf847fb23968
alper gungormusler <anokta@gmail.com>
2023-12-30 20:36:42 -0500

    move scoped_view back in mutable_data

--- barelymusician/internal/mutable_data.h
+++ barelymusician/internal/mutable_data.h
@@ -5,8 +5,6 @@
 #include <memory>
 #include <utility>
 
-#include "barelymusician/internal/scoped_view.h"
-
 namespace barely::internal {
 
 /// Mutable data with a real-time safe view.
@@ -24,6 +22,33 @@ class MutableData {
   void Update(DataType new_data) noexcept;
 
  private:
+  // Scoped immutable view to data.
+  class ScopedView {
+   public:
+    // Constructs new `ScopedView` with `data`.
+    explicit ScopedView(std::atomic<DataType*>& data) noexcept;
+
+    // Destroys `ScopedView`.
+    ~ScopedView() noexcept;
+
+    // Non-copyable and non-movable.
+    ScopedView(const ScopedView& other) noexcept = delete;
+    ScopedView& operator=(const ScopedView& other) noexcept = delete;
+    ScopedView(ScopedView&& other) noexcept = delete;
+    ScopedView& operator=(ScopedView&& other) noexcept = delete;
+
+    // Member access operators.
+    const DataType& operator*() const noexcept;
+    const DataType* operator->() const noexcept;
+
+   private:
+    // Pointer to data.
+    std::atomic<DataType*>& data_;
+
+    // Pointer to view.
+    DataType* view_;
+  };
+
   // Data holder.
   std::unique_ptr<DataType> data_holder_ = std::make_unique<DataType>();
 
@@ -44,6 +69,25 @@ void MutableData<DataType>::Update(DataType new_data) noexcept {
     data = data_holder_.get();
   }
   data_holder_ = std::move(new_data_holder);
+}
+
+template <typename DataType>
+MutableData<DataType>::ScopedView::ScopedView(std::atomic<DataType*>& data) noexcept
+    : data_(data), view_(data_.exchange(nullptr)) {}
+
+template <typename DataType>
+MutableData<DataType>::ScopedView::~ScopedView() noexcept {
+  data_ = view_;
+}
+
+template <typename DataType>
+const DataType& MutableData<DataType>::ScopedView::operator*() const noexcept {
+  return *view_;
+}
+
+template <typename DataType>
+const DataType* MutableData<DataType>::ScopedView::operator->() const noexcept {
+  return view_;
 }
 
 }  // namespace barely::internal

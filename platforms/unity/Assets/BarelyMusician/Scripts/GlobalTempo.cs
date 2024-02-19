3c23d3351a7e130a4fab93b7468e4b7b44bc98ab
alper gungormusler <anokta@gmail.com>
2024-02-19 18:35:47 -0500

    move globaltempo to main assets in unity

---
+++ platforms/unity/Assets/BarelyMusician/Scripts/GlobalTempo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Barely {
+  [DisallowMultipleComponent]
+  public class GlobalTempo : MonoBehaviour {
+    [Tooltip("Sets the global tempo in beats per minute.")]
+    [Range(30.0f, 960.0f)]
+    public double Bpm = 120.0;
+
+    private void Update() {
+      Musician.Tempo = Bpm;
+    }
+  }
+}  // namespace Barely

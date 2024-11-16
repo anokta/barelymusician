41226c29a6770ef0b852e541c2827ff921dc12c0
alper gungormusler <anokta@gmail.com>
2024-11-15 22:36:40 -0500

    minor tweak in physics demo

--- platforms/unity/Assets/Examples/Scripts/Bouncer.cs
+++ platforms/unity/Assets/Examples/Scripts/Bouncer.cs
@@ -5,9 +5,16 @@ namespace Barely {
     public class Bouncer : MonoBehaviour {
       public Instrument instrument;
       public Scale scale;
+      public Sparkler sparkler;
 
       private int _lastIndex = 0;
       private double _lastPitch = 0.0;
+
+      private void Awake() {
+        Color.RGBToHSV(sparkler.noteOnColor, out float h, out float s, out float v);
+        h = Random.Range(0.3f, 1.0f);
+        sparkler.noteOnColor = Color.HSVToRGB(h, s, v);
+      }
 
       private void Update() {
         if (transform.position.y < -100.0f) {

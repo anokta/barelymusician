116f926e34fdba88b80b6125823d24710e0f7347
alper gungormusler <anokta@gmail.com>
2025-02-22 01:44:32 -0500

    remove unused one_pole_filter
    closed #8

--- src/dsp/biquad_filter_test.cpp
+++ src/dsp/biquad_filter_test.cpp
@@ -16,7 +16,7 @@ constexpr float kSampleInterval = 1.0f / kSampleRate;
 constexpr float kFilterQ = 0.5f;
 
 // Tests that a low-pass filter does not alter the input when the coefficient is set to all-pass.
-TEST(OnePoleFilterTest, LowPassAllPass) {
+TEST(BiquadFilterTest, LowPassAllPass) {
   const auto coeffs =
       GetFilterCoefficients(kSampleInterval, FilterType::kLowPass, kSampleRate, kFilterQ);
 
@@ -27,7 +27,7 @@ TEST(OnePoleFilterTest, LowPassAllPass) {
 }
 
 // Tests that a high-pass filter does not alter the input when the coefficient is set to all-pass.
-TEST(OnePoleFilterTest, HighPassAllPass) {
+TEST(BiquadFilterTest, HighPassAllPass) {
   const auto coeffs = GetFilterCoefficients(kSampleInterval, FilterType::kHighPass, 0.0f, kFilterQ);
 
   BiquadFilter filter;

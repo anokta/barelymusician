85949147a58e49d5ce1441de0d5edf14d75d67a1
alper gungormusler <anokta@gmail.com>
2025-05-24 23:11:48 -0400

    add main wasm functionality
    audio process wip

--- platforms/wasm/wasm.cpp
+++ platforms/wasm/wasm.cpp
@@ -5,23 +5,90 @@
 using ::barely::ControlType;
 using ::barely::Engine;
 using ::barely::Instrument;
+using ::barely::Performer;
+using ::barely::Task;
+using ::barely::TaskState;
+using ::barely::Trigger;
+using ::emscripten::class_;
+using ::emscripten::optional_override;
+using ::emscripten::val;
+using ::emscripten::return_value_policy::take_ownership;
+
+static Instrument Engine_CreateInstrument(Engine& engine) noexcept {
+  return engine.CreateInstrument();
+}
+
+static void Instrument_SetNoteOn(Instrument& instrument, float pitch) noexcept {
+  instrument.SetNoteOn(pitch);
+}
 
 EMSCRIPTEN_BINDINGS(barelymusician) {
-  emscripten::class_<Engine>("Engine")
+  class_<Engine>("Engine")
       .constructor<int, float>()
+      .function("createInstrument", &Engine_CreateInstrument, take_ownership())
+      .function("createPerformer", &Engine::CreatePerformer, take_ownership())
       .function("generateRandomNumber",
                 static_cast<double (Engine::*)()>(&Engine::GenerateRandomNumber))
-      .function("getTimestamp", &Engine::GetTimestamp)
+      .function("generateRandomNumber", &Engine::GenerateRandomNumber<double>)
+      .function("generateRandomInteger", &Engine::GenerateRandomNumber<int>)
       .function("update", &Engine::Update)
       .property("seed", &Engine::GetSeed, &Engine::SetSeed)
-      .property("tempo", &Engine::GetTempo, &Engine::SetTempo);
+      .property("tempo", &Engine::GetTempo, &Engine::SetTempo)
+      .property("timestamp", &Engine::GetTimestamp);
 
-  emscripten::class_<Instrument>("Instrument")
+  class_<Instrument>("Instrument")
+      .function("getControl", &Instrument::GetControl<float>)
+      .function("getNoteControl", &Instrument::GetNoteControl<float>)
       .function("isNoteOn", &Instrument::IsNoteOn)
+      .function("process", &Instrument::Process)
       .function("setAllNotesOff", &Instrument::SetAllNotesOff)
-      .function("setControl",
-                static_cast<void (Instrument::*)(ControlType, float)>(&Instrument::SetControl))
+      .function("setControl", &Instrument::SetControl<float>)
+      .function("setNoteControl", &Instrument::SetNoteControl<float>)
       .function("setNoteOff", &Instrument::SetNoteOff)
+      .function("setNoteOffCallback",
+                optional_override([](Instrument& instrument, val js_callback) {
+                  return instrument.SetNoteOffCallback(
+                      [js_callback](float pitch) { js_callback(pitch); });
+                }))
+      .function("setNoteOn", &Instrument_SetNoteOn)
       .function("setNoteOn",
-                static_cast<void (Instrument::*)(float, float, float)>(&Instrument::SetNoteOn));
+                static_cast<void (Instrument::*)(float, float)>(&Instrument::SetNoteOn))
+      .function("setNoteOn",
+                static_cast<void (Instrument::*)(float, float, float)>(&Instrument::SetNoteOn))
+      .function(
+          "setNoteOnCallback", optional_override([](Instrument& instrument, val js_callback) {
+            return instrument.SetNoteOnCallback([js_callback](float pitch) { js_callback(pitch); });
+          }));
+  // TODO(#164): Add sample data support.
+
+  class_<Performer>("Performer")
+      .function("createTask", &Performer::CreateTask, take_ownership())
+      .function("createTrigger",
+                optional_override([](Performer& performer, double position, val js_callback) {
+                  return performer.CreateTrigger(position, [js_callback]() { js_callback(); });
+                }),
+                take_ownership())
+      .function("start", &Performer::Start)
+      .function("stop", &Performer::Stop)
+      .property("loopBeginPosition", &Performer::GetLoopBeginPosition,
+                &Performer::SetLoopBeginPosition)
+      .property("loopLength", &Performer::GetLoopLength, &Performer::SetLoopLength)
+      .property("position", &Performer::GetPosition, &Performer::SetPosition)
+      .property("looping", &Performer::IsLooping, &Performer::SetLooping)
+      .property("isPlaying", &Performer::IsPlaying);
+
+  class_<Task>("Task")
+      .function(
+          "setProcessCallback", optional_override([](Task& task, val js_callback) {
+            return task.SetProcessCallback([js_callback](TaskState state) { js_callback(state); });
+          }))
+      .property("isActive", &Task::IsActive)
+      .property("duration", &Task::GetDuration, &Task::SetDuration)
+      .property("position", &Task::GetPosition, &Task::SetPosition);
+
+  class_<Trigger>("Trigger")
+      .function("setProcessCallback", optional_override([](Trigger& trigger, val js_callback) {
+                  return trigger.SetProcessCallback([js_callback]() { js_callback(); });
+                }))
+      .property("position", &Trigger::GetPosition, &Trigger::SetPosition);
 }

4a6ade06033af767c9ba512ae887f252e8a8fac6
alper gungormusler <anokta@gmail.com>
2026-01-08 22:07:15 -0500

    remove arp state

--- src/engine/instrument_controller.cpp
+++ src/engine/instrument_controller.cpp
@@ -120,13 +120,13 @@ void InstrumentController::SetAllNotesOff() noexcept {
 void InstrumentController::SetAllNotesOff(uint32_t instrument_index) noexcept {
   auto& instrument = engine_.instrument_pool.Get(instrument_index);
   instrument.note_controls.clear();
-  if (instrument.arp.is_active) {
+  if (instrument.is_arp_pitch_on) {
     instrument.pitches.clear();
-    instrument.arp.is_active = false;
-    instrument.arp.phase = 0.0;
-    instrument.arp.pitch_index = -1;
-    instrument.note_event_callback(BarelyNoteEventType_kEnd, instrument.arp.pitch);
-    engine_.ScheduleMessage(NoteOffMessage{instrument_index, instrument.arp.pitch});
+    instrument.is_arp_pitch_on = false;
+    instrument.arp_phase = 0.0;
+    instrument.arp_pitch_index = -1;
+    instrument.note_event_callback(BarelyNoteEventType_kEnd, instrument.arp_pitch);
+    engine_.ScheduleMessage(NoteOffMessage{instrument_index, instrument.arp_pitch});
   } else {
     for (const float pitch : std::exchange(instrument.pitches, {})) {
       instrument.note_event_callback(BarelyNoteEventType_kEnd, pitch);
@@ -142,19 +142,19 @@ void InstrumentController::SetControl(uint32_t instrument_index, BarelyInstrumen
     switch (type) {
       case BarelyInstrumentControlType_kArpMode:
         if (static_cast<BarelyArpMode>(value) == BarelyArpMode_kNone) {
-          if (instrument.arp.pitch_index != -1) {
-            instrument.arp.is_active = false;
-            instrument.arp.phase = 0.0;
-            instrument.arp.pitch_index = -1;
-            instrument.note_event_callback(BarelyNoteEventType_kEnd, instrument.arp.pitch);
-            engine_.ScheduleMessage(NoteOffMessage{instrument_index, instrument.arp.pitch});
+          if (instrument.arp_pitch_index != -1) {
+            instrument.is_arp_pitch_on = false;
+            instrument.arp_phase = 0.0;
+            instrument.arp_pitch_index = -1;
+            instrument.note_event_callback(BarelyNoteEventType_kEnd, instrument.arp_pitch);
+            engine_.ScheduleMessage(NoteOffMessage{instrument_index, instrument.arp_pitch});
             for (const auto& [pitch, note_controls] : instrument.note_controls) {
               instrument.note_event_callback(BarelyNoteEventType_kBegin, pitch);
               engine_.ScheduleMessage(
                   NoteOnMessage{instrument_index, pitch, BuildNoteControls(note_controls)});
             }
           }
-        } else if (!instrument.pitches.empty() && !instrument.arp.is_active) {
+        } else if (!instrument.pitches.empty() && !instrument.is_arp_pitch_on) {
           for (const float pitch : instrument.pitches) {
             instrument.note_event_callback(BarelyNoteEventType_kEnd, pitch);
             engine_.ScheduleMessage(NoteOffMessage{instrument_index, pitch});
@@ -195,11 +195,11 @@ void InstrumentController::SetNoteOff(uint32_t instrument_index, float pitch) no
     instrument.pitches.erase(
         std::find(instrument.pitches.begin(), instrument.pitches.end(), pitch));
     if (instrument.pitches.empty() && instrument.IsArpEnabled()) {
-      instrument.arp.is_active = false;
-      instrument.arp.phase = 0.0;
-      instrument.arp.pitch_index = -1;
-      instrument.note_event_callback(BarelyNoteEventType_kEnd, instrument.arp.pitch);
-      engine_.ScheduleMessage(NoteOffMessage{instrument_index, instrument.arp.pitch});
+      instrument.is_arp_pitch_on = false;
+      instrument.arp_phase = 0.0;
+      instrument.arp_pitch_index = -1;
+      instrument.note_event_callback(BarelyNoteEventType_kEnd, instrument.arp_pitch);
+      engine_.ScheduleMessage(NoteOffMessage{instrument_index, instrument.arp_pitch});
     } else if (!instrument.IsArpEnabled()) {
       instrument.note_event_callback(BarelyNoteEventType_kEnd, pitch);
       engine_.ScheduleMessage(NoteOffMessage{instrument_index, pitch});
@@ -249,41 +249,41 @@ void InstrumentController::ProcessArp(MainRng& main_rng) noexcept {
     if (!instrument.IsArpEnabled() || instrument.pitches.empty()) {
       return;
     }
-    if (!instrument.arp.is_active && instrument.arp.phase == 0.0) {
+    if (!instrument.is_arp_pitch_on && instrument.arp_phase == 0.0) {
       const int size = static_cast<int>(instrument.pitches.size());
       switch (static_cast<BarelyArpMode>(
           instrument.controls[BarelyInstrumentControlType_kArpMode].value)) {
         case BarelyArpMode_kUp:
-          instrument.arp.pitch_index = (instrument.arp.pitch_index + 1) % size;
+          instrument.arp_pitch_index = (instrument.arp_pitch_index + 1) % size;
           break;
         case BarelyArpMode_kDown:
-          instrument.arp.pitch_index = (instrument.arp.pitch_index == -1)
+          instrument.arp_pitch_index = (instrument.arp_pitch_index == -1)
                                            ? size - 1
-                                           : (instrument.arp.pitch_index + size - 1) % size;
+                                           : (instrument.arp_pitch_index + size - 1) % size;
           break;
         case BarelyArpMode_kRandom:
-          instrument.arp.pitch_index = main_rng.Generate(0, size);
+          instrument.arp_pitch_index = main_rng.Generate(0, size);
           break;
         default:
           assert(!"Invalid arpeggiator mode");
           return;
       }
-      assert(instrument.arp.pitch_index >= 0 && instrument.arp.pitch_index < size);
-      instrument.arp.pitch = instrument.pitches[instrument.arp.pitch_index];
+      assert(instrument.arp_pitch_index >= 0 && instrument.arp_pitch_index < size);
+      instrument.arp_pitch = instrument.pitches[instrument.arp_pitch_index];
 
-      instrument.arp.is_active = true;
-      instrument.note_event_callback(BarelyNoteEventType_kBegin, instrument.arp.pitch);
+      instrument.is_arp_pitch_on = true;
+      instrument.note_event_callback(BarelyNoteEventType_kBegin, instrument.arp_pitch);
       engine_.ScheduleMessage(
-          NoteOnMessage{engine_.instrument_pool.GetActiveIndex(i), instrument.arp.pitch,
-                        BuildNoteControls(instrument.note_controls.at(instrument.arp.pitch))});
-    } else if (instrument.arp.is_active &&
-               instrument.arp.phase ==
+          NoteOnMessage{engine_.instrument_pool.GetActiveIndex(i), instrument.arp_pitch,
+                        BuildNoteControls(instrument.note_controls.at(instrument.arp_pitch))});
+    } else if (instrument.is_arp_pitch_on &&
+               instrument.arp_phase ==
                    static_cast<double>(
                        instrument.controls[BarelyInstrumentControlType_kArpGateRatio].value)) {
-      instrument.arp.is_active = false;
-      instrument.note_event_callback(BarelyNoteEventType_kEnd, instrument.arp.pitch);
+      instrument.is_arp_pitch_on = false;
+      instrument.note_event_callback(BarelyNoteEventType_kEnd, instrument.arp_pitch);
       engine_.ScheduleMessage(
-          NoteOffMessage{engine_.instrument_pool.GetActiveIndex(i), instrument.arp.pitch});
+          NoteOffMessage{engine_.instrument_pool.GetActiveIndex(i), instrument.arp_pitch});
     }
   }
 }
@@ -297,17 +297,8 @@ void InstrumentController::Update(double duration) noexcept {
 double InstrumentController::GetNextDuration() const noexcept {
   double next_duration = std::numeric_limits<double>::max();
   for (uint32_t i = 0; i < engine_.instrument_pool.GetActiveCount(); ++i) {
-    auto& instrument = engine_.instrument_pool.GetActive(i);
-    if (!instrument.IsArpEnabled() || instrument.pitches.empty()) {
-      continue;
-    }
-    if (const auto maybe_next_duration = instrument.arp.GetNextDuration(
-            static_cast<double>(instrument.controls[BarelyInstrumentControlType_kArpRate].value),
-            static_cast<double>(
-                instrument.controls[BarelyInstrumentControlType_kArpGateRatio].value));
-        maybe_next_duration.has_value() && *maybe_next_duration < next_duration) {
-      next_duration = *maybe_next_duration;
-    }
+    next_duration =
+        std::min(engine_.instrument_pool.GetActive(i).GetNextArpDuration(), next_duration);
   }
   return next_duration;
 }
--- src/engine/instrument_state.h
+++ src/engine/instrument_state.h
@@ -3,9 +3,12 @@
 
 #include <barelymusician.h>
 
+#include <cassert>
+#include <cmath>
+#include <limits>
+
 #include "core/callback.h"
 #include "dsp/control.h"
-#include "engine/arp_state.h"
 
 namespace barely {
 
@@ -17,18 +20,37 @@ struct InstrumentState {
   // Map of note control arrays by their pitches.
   std::unordered_map<float, barely::NoteControlArray> note_controls = {};
 
-  // Arpeggiator state.
-  ArpState arp = {};
-
   // Note event callback.
   Callback<BarelyNoteEventCallback> note_event_callback = {};
 
+  // Arpeggiator state.
+  double arp_phase = 0.0;
+  float arp_pitch = 0.0f;
+  int arp_pitch_index = -1;
+  bool is_arp_pitch_on = false;
+
   void Update(double duration) noexcept {
     if (IsArpEnabled() && !pitches.empty()) {
-      arp.Update(duration,
-                 static_cast<double>(controls[BarelyInstrumentControlType_kArpRate].value),
-                 static_cast<double>(controls[BarelyInstrumentControlType_kArpGateRatio].value));
+      assert(duration <= GetNextArpDuration());
+      const double rate = static_cast<double>(controls[BarelyInstrumentControlType_kArpRate].value);
+      arp_phase = std::fmod(arp_phase + duration * rate, 1.0);
     }
+  }
+
+  double GetNextArpDuration() const noexcept {
+    if (!IsArpEnabled() || pitches.empty()) {
+      return std::numeric_limits<double>::max();
+    }
+    const double rate = static_cast<double>(controls[BarelyInstrumentControlType_kArpRate].value);
+    if (rate <= 0.0) {
+      return std::numeric_limits<double>::max();
+    }
+    const double ratio =
+        static_cast<double>(controls[BarelyInstrumentControlType_kArpGateRatio].value);
+    if (!is_arp_pitch_on) {
+      return (arp_phase < ratio) ? 0.0 : (1.0 - arp_phase) / rate;
+    }
+    return (ratio - arp_phase) / rate;
   }
 
   /// Returns whether the arpeggiator is enabled.
@@ -39,7 +61,7 @@ struct InstrumentState {
 
   /// Returns whether a note is on or not.
   bool IsNoteOn(float pitch) const noexcept {
-    return arp.is_active ? (arp.pitch == pitch) : note_controls.contains(pitch);
+    return is_arp_pitch_on ? (arp_pitch == pitch) : note_controls.contains(pitch);
   }
 };
 

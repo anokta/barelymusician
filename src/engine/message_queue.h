ecd546f04867ea2f62234f25b6e610ebd335f29f
alper gungormusler <anokta@gmail.com>
2026-01-16 13:13:04 -0500

    relax message queue memory order

--- src/engine/message_queue.h
+++ src/engine/message_queue.h
@@ -19,13 +19,13 @@ class MessageQueue {
   /// @param message Message.
   /// @return True if successful, false otherwise.
   bool Add(int64_t message_frame, Message message) noexcept {
-    const int index = write_index_;
+    const int index = write_index_.load(std::memory_order_relaxed);
     const int next_index = (index + 1) % kMaxMessageCount;
-    if (next_index == read_index_) {
+    if (next_index == read_index_.load(std::memory_order_acquire)) {
       return false;
     }
     messages_[index] = {message_frame, std::move(message)};
-    write_index_ = next_index;
+    write_index_.store(next_index, std::memory_order_release);
     return true;
   }
 
@@ -34,11 +34,12 @@ class MessageQueue {
   /// @param end_frame End frame.
   /// @return Pointer to message if successful, `nullptr` otherwise.
   std::pair<int64_t, Message>* GetNext(int64_t end_frame) noexcept {
-    const int index = read_index_;
-    if (index == write_index_ || messages_[index].first >= end_frame) {
+    const int index = read_index_.load(std::memory_order_relaxed);
+    if (index == write_index_.load(std::memory_order_acquire) ||
+        messages_[index].first >= end_frame) {
       return nullptr;
     }
-    read_index_ = (index + 1) % kMaxMessageCount;
+    read_index_.store((index + 1) % kMaxMessageCount, std::memory_order_release);
     return &messages_[index];
   }
 

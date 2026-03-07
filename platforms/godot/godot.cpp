125ff833f3bc31e4021519f22d1ddbaee0fbfaa3
alper gungormusler <anokta@gmail.com>
2026-03-07 00:56:11 -0500

    add audio playback module

--- platforms/godot/godot.cpp
+++ platforms/godot/godot.cpp
@@ -1,31 +1,34 @@
 #include <barelymusician.h>
 
-#include <memory>
+#include <cstdint>
 
 #include "gdextension_interface.h"
 #include "godot_cpp/classes/audio_server.hpp"
+#include "godot_cpp/classes/audio_stream.hpp"
+#include "godot_cpp/classes/audio_stream_playback.hpp"
+#include "godot_cpp/classes/audio_stream_playback_resampled.hpp"
 #include "godot_cpp/classes/engine.hpp"
 #include "godot_cpp/classes/node.hpp"
 #include "godot_cpp/classes/object.hpp"
 #include "godot_cpp/core/class_db.hpp"
 #include "godot_cpp/core/error_macros.hpp"
 #include "godot_cpp/core/method_bind.hpp"
+#include "godot_cpp/variant/utility_functions.hpp"
 
+using ::godot::AudioFrame;
 using ::godot::AudioServer;
+using ::godot::AudioStream;
+using ::godot::AudioStreamPlayback;
+using ::godot::AudioStreamPlaybackResampled;
 using ::godot::ClassDB;
 using ::godot::D_METHOD;
 using ::godot::GDExtensionBinding;
 using ::godot::ModuleInitializationLevel;
 using ::godot::Object;
+using ::godot::Ref;
 
 class BarelyMusician : public Object {
  public:
-  static void create() {
-    if (const AudioServer* audio_server = AudioServer::get_singleton()) {
-      BarelyEngine_Create(audio_server->get_mix_rate(), &engine_);
-    }
-  }
-
   static void destroy() {
     BarelyEngine_Destroy(engine_);
     engine_ = nullptr;
@@ -33,7 +36,15 @@ class BarelyMusician : public Object {
 
   static BarelyEngine* get() {
     if (engine_ == nullptr) {
-      create();
+      // TODO(#181): Support sample rate changes after initialization.
+      BarelyEngine_Create(static_cast<int32_t>(AudioServer::get_singleton()->get_mix_rate()),
+                          &engine_);
+      // todo: test
+      uint32_t instrument_id = 0;
+      BarelyEngine_CreateInstrument(engine_, &instrument_id);
+      BarelyInstrument_SetNoteOn(engine_, instrument_id, 0.0f);
+      BarelyInstrument_SetControl(engine_, instrument_id, BarelyInstrumentControlType_kOscMix,
+                                  0.5f);
     }
     return engine_;
   }
@@ -71,12 +82,58 @@ class BarelyMusician : public Object {
   static inline BarelyEngine* engine_ = nullptr;  // singleton
 };
 
+class BarelyAudioStreamPlayback : public AudioStreamPlaybackResampled {
+ public:
+  BarelyAudioStreamPlayback() {
+    sample_rate_ = static_cast<double>(AudioServer::get_singleton()->get_mix_rate());
+  }
+
+  float _get_stream_sampling_rate() const override { return static_cast<float>(sample_rate_); }
+
+  bool _is_playing() const override { return true; }
+
+  int32_t _mix_resampled(AudioFrame* buffer, int32_t frames) override {
+    static constexpr int32_t kStereoChannelCount = 2;
+    float output[BARELY_MAX_FRAME_COUNT * kStereoChannelCount];
+    BarelyEngine_Process(BarelyMusician::get(), output, kStereoChannelCount, frames, timestamp_);
+    for (int32_t frame = 0; frame < frames; ++frame) {
+      buffer[frame].left = output[frame * kStereoChannelCount];
+      buffer[frame].right = output[frame * kStereoChannelCount + 1];
+    }
+    timestamp_ += static_cast<double>(frames) / sample_rate_;
+    return frames;
+  }
+
+ private:
+  GDCLASS(BarelyAudioStreamPlayback, AudioStreamPlaybackResampled);
+
+  static void _bind_methods() {}
+
+  double sample_rate_ = 0.0;
+  double timestamp_ = 0.0;
+};
+
+class BarelyAudioStream : public AudioStream {
+ public:
+  Ref<AudioStreamPlayback> _instantiate_playback() const override {
+    Ref<BarelyAudioStreamPlayback> playback;
+    playback.instantiate();
+    return playback;
+  }
+
+ private:
+  GDCLASS(BarelyAudioStream, AudioStream);
+
+  static void _bind_methods() {}
+};
+
 void initialize_barelymusiciangodot(ModuleInitializationLevel p_level) {
   if (p_level != ModuleInitializationLevel::MODULE_INITIALIZATION_LEVEL_SCENE) {
     return;
   }
   ClassDB::register_class<BarelyMusician>();
-  BarelyMusician::create();
+  ClassDB::register_class<BarelyAudioStream>();
+  ClassDB::register_class<BarelyAudioStreamPlayback>();
 }
 
 void terminate_barelymusiciangodot(ModuleInitializationLevel p_level) {

0353d7b9f8ec2a7b2255ba41dfb7080c801d50bf
alper gungormusler <anokta@gmail.com>
2026-03-06 23:22:48 -0500

    initial skeleton cleanup

--- platforms/godot/godot.cpp
+++ platforms/godot/godot.cpp
@@ -1,59 +1,101 @@
 #include <barelymusician.h>
 
-#include <godot_cpp/classes/audio_server.hpp>
-#include <godot_cpp/classes/node.hpp>
-#include <godot_cpp/core/class_db.hpp>
-#include <godot_cpp/core/error_macros.hpp>
-#include <godot_cpp/godot.hpp>
+#include <memory>
 
-using namespace godot;
+#include "gdextension_interface.h"
+#include "godot_cpp/classes/audio_server.hpp"
+#include "godot_cpp/classes/engine.hpp"
+#include "godot_cpp/classes/node.hpp"
+#include "godot_cpp/classes/object.hpp"
+#include "godot_cpp/core/class_db.hpp"
+#include "godot_cpp/core/error_macros.hpp"
+#include "godot_cpp/core/method_bind.hpp"
 
-class BarelyMusician : public Node {
-  GDCLASS(BarelyMusician, Node)
+using ::godot::AudioServer;
+using ::godot::ClassDB;
+using ::godot::D_METHOD;
+using ::godot::GDExtensionBinding;
+using ::godot::ModuleInitializationLevel;
+using ::godot::Object;
 
+class BarelyMusician : public Object {
  public:
-  BarelyMusician() {
+  static void create() {
     if (const AudioServer* audio_server = AudioServer::get_singleton()) {
-      BarelyEngine_Create(audio_server->get_mix_rate(), &engine);
+      BarelyEngine_Create(audio_server->get_mix_rate(), &engine_);
     }
   }
 
-  ~BarelyMusician() { BarelyEngine_Destroy(engine); }
+  static void destroy() {
+    BarelyEngine_Destroy(engine_);
+    engine_ = nullptr;
+  }
 
-  double get_tempo() const {
+  static BarelyEngine* get() {
+    if (engine_ == nullptr) {
+      create();
+    }
+    return engine_;
+  }
+
+  static double get_tempo() {
     double tempo = 0.0;
-    BarelyEngine_GetTempo(engine, &tempo);
+    BarelyEngine_GetTempo(get(), &tempo);
     return tempo;
   }
 
-  void set_tempo(double tempo) { BarelyEngine_SetTempo(engine, tempo); }
+  static double get_timestamp() {
+    double timestamp = 0.0;
+    BarelyEngine_GetTimestamp(get(), &timestamp);
+    return timestamp;
+  }
 
- protected:
-  static void _bind_methods();
+  static void set_tempo(double tempo) { BarelyEngine_SetTempo(get(), tempo); }
+
+  static void update(double timestamp) { BarelyEngine_Update(get(), timestamp); }
 
  private:
-  BarelyEngine* engine = nullptr;
+  GDCLASS(BarelyMusician, Object);
+
+  static void _bind_methods() {
+    ClassDB::bind_static_method(kClassName, D_METHOD("get_tempo"), &BarelyMusician::get_tempo);
+    ClassDB::bind_static_method(kClassName, D_METHOD("get_timestamp"),
+                                &BarelyMusician::get_timestamp);
+    ClassDB::bind_static_method(kClassName, D_METHOD("set_tempo", "tempo"),
+                                &BarelyMusician::set_tempo);
+    ClassDB::bind_static_method(kClassName, D_METHOD("update", "timestamp"),
+                                &BarelyMusician::update);
+  }
+
+  static constexpr const char* kClassName = "BarelyMusician";
+  static inline BarelyEngine* engine_ = nullptr;  // singleton
 };
 
-void BarelyMusician::_bind_methods() {
-  ClassDB::bind_method(D_METHOD("get_tempo"), &BarelyMusician::get_tempo);
-  ClassDB::bind_method(D_METHOD("set_tempo", "tempo"), &BarelyMusician::set_tempo);
-}
-
-static void initialize_barelymusiciangodot_module(ModuleInitializationLevel p_level) {
-  if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+void initialize_barelymusiciangodot(ModuleInitializationLevel p_level) {
+  if (p_level != ModuleInitializationLevel::MODULE_INITIALIZATION_LEVEL_SCENE) {
     return;
   }
   ClassDB::register_class<BarelyMusician>();
+  BarelyMusician::create();
+}
+
+void terminate_barelymusiciangodot(ModuleInitializationLevel p_level) {
+  if (p_level != ModuleInitializationLevel::MODULE_INITIALIZATION_LEVEL_SCENE) {
+    return;
+  }
+  BarelyMusician::destroy();
 }
 
 extern "C" {
 
-GDExtensionBool GDE_EXPORT barelymusiciangodot_library_init(
+GDExtensionBool GDE_EXPORT barelymusiciangodot_init(
     GDExtensionInterfaceGetProcAddress p_get_proc_address,
     const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization* r_initialization) {
-  godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
-  init_obj.register_initializer(initialize_barelymusiciangodot_module);
+  GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
+  init_obj.register_initializer(initialize_barelymusiciangodot);
+  init_obj.register_terminator(terminate_barelymusiciangodot);
+  init_obj.set_minimum_library_initialization_level(
+      ModuleInitializationLevel::MODULE_INITIALIZATION_LEVEL_SCENE);
   return init_obj.init();
 }
 

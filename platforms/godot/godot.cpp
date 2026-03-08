f6aafda866859ac8b2f5feea3691a82ae087a3a5
alper gungormusler <anokta@gmail.com>
2026-03-07 19:47:42 -0500

    add main loop update module

--- platforms/godot/godot.cpp
+++ platforms/godot/godot.cpp
@@ -1,5 +1,6 @@
 #include <barelymusician.h>
 
+#include <atomic>
 #include <cstdint>
 
 #include "gdextension_interface.h"
@@ -10,6 +11,8 @@
 #include "godot_cpp/classes/engine.hpp"
 #include "godot_cpp/classes/node.hpp"
 #include "godot_cpp/classes/object.hpp"
+#include "godot_cpp/classes/scene_tree.hpp"
+#include "godot_cpp/classes/window.hpp"
 #include "godot_cpp/core/class_db.hpp"
 #include "godot_cpp/core/error_macros.hpp"
 #include "godot_cpp/core/method_bind.hpp"
@@ -22,10 +25,13 @@ using ::godot::AudioStreamPlayback;
 using ::godot::AudioStreamPlaybackResampled;
 using ::godot::ClassDB;
 using ::godot::D_METHOD;
+using ::godot::Engine;
 using ::godot::GDExtensionBinding;
 using ::godot::ModuleInitializationLevel;
+using ::godot::Node;
 using ::godot::Object;
 using ::godot::Ref;
+using ::godot::SceneTree;
 
 class BarelyMusician : public Object {
  public:
@@ -39,7 +45,7 @@ class BarelyMusician : public Object {
       // TODO(#181): Support sample rate changes after initialization.
       BarelyEngine_Create(static_cast<int32_t>(AudioServer::get_singleton()->get_mix_rate()),
                           &engine_);
-      // todo: test
+      // TODO(#181): temp testing
       uint32_t instrument_id = 0;
       BarelyEngine_CreateInstrument(engine_, &instrument_id);
       BarelyInstrument_SetNoteOn(engine_, instrument_id, 0.0f);
@@ -63,10 +69,11 @@ class BarelyMusician : public Object {
 
   static void set_tempo(double tempo) { BarelyEngine_SetTempo(get(), tempo); }
 
-  static void update(double timestamp) { BarelyEngine_Update(get(), timestamp); }
-
  private:
   GDCLASS(BarelyMusician, Object);
+
+  BarelyMusician() = default;
+  ~BarelyMusician() = default;
 
   static void _bind_methods() {
     ClassDB::bind_static_method(kClassName, D_METHOD("get_tempo"), &BarelyMusician::get_tempo);
@@ -74,8 +81,6 @@ class BarelyMusician : public Object {
                                 &BarelyMusician::get_timestamp);
     ClassDB::bind_static_method(kClassName, D_METHOD("set_tempo", "tempo"),
                                 &BarelyMusician::set_tempo);
-    ClassDB::bind_static_method(kClassName, D_METHOD("update", "timestamp"),
-                                &BarelyMusician::update);
   }
 
   static constexpr const char* kClassName = "BarelyMusician";
@@ -84,6 +89,8 @@ class BarelyMusician : public Object {
 
 class BarelyAudioStreamPlayback : public AudioStreamPlaybackResampled {
  public:
+  static double get_audio_timestamp() { return timestamp_.load(std::memory_order_relaxed); }
+
   BarelyAudioStreamPlayback() {
     sample_rate_ = static_cast<double>(AudioServer::get_singleton()->get_mix_rate());
   }
@@ -100,31 +107,59 @@ class BarelyAudioStreamPlayback : public AudioStreamPlaybackResampled {
       buffer[frame].left = output[frame * kStereoChannelCount];
       buffer[frame].right = output[frame * kStereoChannelCount + 1];
     }
-    timestamp_ += static_cast<double>(frames) / sample_rate_;
+    frame_ += static_cast<int64_t>(frames);
+    timestamp_ = static_cast<double>(frame_) / sample_rate_;
     return frames;
   }
 
+  void _start([[maybe_unused]] double p_from_pos) override {}
+
  private:
   GDCLASS(BarelyAudioStreamPlayback, AudioStreamPlaybackResampled);
-
   static void _bind_methods() {}
 
+  static_assert(std::atomic<double>::is_always_lock_free);
+  static inline std::atomic<double> timestamp_ = 0.0;
+
+  int64_t frame_ = 0;
   double sample_rate_ = 0.0;
-  double timestamp_ = 0.0;
+};
+
+class BarelyMusicianNode : public Node {
+ public:
+  void _process([[maybe_unused]] double delta) override {
+    static constexpr double kLatency = 0.2;
+    BarelyEngine_Update(BarelyMusician::get(),
+                        BarelyAudioStreamPlayback::get_audio_timestamp() + kLatency);
+  }
+
+ private:
+  GDCLASS(BarelyMusicianNode, Node);
+  static void _bind_methods() {}
 };
 
 class BarelyAudioStream : public AudioStream {
  public:
   Ref<AudioStreamPlayback> _instantiate_playback() const override {
-    Ref<BarelyAudioStreamPlayback> playback;
-    playback.instantiate();
-    return playback;
+    if (!playback_.is_valid()) {
+      if (SceneTree* tree = Object::cast_to<SceneTree>(Engine::get_singleton()->get_main_loop())) {
+        static constexpr const char* kNodeName = "BarelyMusicianNode";
+        if (!tree->get_root()->has_node(kNodeName)) {
+          BarelyMusicianNode* node = memnew(BarelyMusicianNode);
+          node->set_name(kNodeName);
+          tree->get_root()->call_deferred("add_child", node);
+        }
+        playback_.instantiate();
+      }
+    }
+    return playback_;
   }
 
  private:
   GDCLASS(BarelyAudioStream, AudioStream);
-
   static void _bind_methods() {}
+
+  static inline Ref<BarelyAudioStreamPlayback> playback_ = {};  // singleton
 };
 
 void initialize_barelymusiciangodot(ModuleInitializationLevel p_level) {
@@ -133,7 +168,8 @@ void initialize_barelymusiciangodot(ModuleInitializationLevel p_level) {
   }
   ClassDB::register_class<BarelyMusician>();
   ClassDB::register_class<BarelyAudioStream>();
-  ClassDB::register_class<BarelyAudioStreamPlayback>();
+  ClassDB::register_internal_class<BarelyMusicianNode>();
+  ClassDB::register_internal_class<BarelyAudioStreamPlayback>();
 }
 
 void terminate_barelymusiciangodot(ModuleInitializationLevel p_level) {
